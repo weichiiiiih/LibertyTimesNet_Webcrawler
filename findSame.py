aaf60b6b5aeb4fb7cd33542248efd07ff3cbd4f5
weichiiiiih <144878579+weichiiiiih@users.noreply.github.com>
2024-11-15 22:52:42 +0800

    Update findSame.py

--- findSame.py
+++ findSame.py
@@ -1,5 +1,5 @@
-#兩個檔案找相同的，不相同的存
-# 到新檔案裡
+# 依據爬蟲結果，在兩個檔案中找到相同的篇名，並將不相同的部分存到新檔案裡
+# 範例：檔案1的關鍵字為「日本」、檔案2的關鍵字為京都
 def read_titles_from_file(file_path):
     titles = {}
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -45,19 +45,20 @@ def filter_file(input_file_path, output_file_path, matching_indices):
                 if line.startswith('發表時間：'):
                     write_line = True  # Reset flag after writing complete item
 
-# 文件路径
-file1_path = '/Users/weichiiiiih/Desktop/Cword_mainland_all拷貝.txt'
-file2_path = '/Users/weichiiiiih/Desktop/Cword_mainlander_all拷貝.txt'
-output_file_path = '/Users/weichiiiiih/Desktop/mainlander_de.txt'
+# 更改檔案路径
+# 範例：檔案1的關鍵字為「日本」、檔案2的關鍵字為京都
+file1_path = '/Users/Desktop/keyword_Japan.txt'
+file2_path = '/Users/Desktop/keyword_Kyoto.txt'
+output_file_path = '/Users/Desktop/Japan_Kyoto_de.txt'
 
-# 读取文件标题
+# 獲取檔案標題
 file1_titles = read_titles_from_file(file1_path)
 file2_titles = read_titles_from_file(file2_path)
 
-# 获取匹配的项目编号
+# 獲取匹配的項目編號
 matching_indices = get_matching_indices(file1_titles, file2_titles)
 
-# 过滤文件并写入新文件
+# 過濾檔案並寫入新檔案
 filter_file(file2_path, output_file_path, matching_indices)
 
 print(f"Filtered content has been written to {output_file_path}")
